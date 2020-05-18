cee452a59685c1656a4521bf6e91b845564be49a
Alberto Oshiro <albertooshiro90@gmail.com>
2020-05-18 12:05:51 -0700

    minor changes

--- Root/Backend/src/routes/api/api-test-routes.js
+++ Root/Backend/src/routes/api/api-test-routes.js
@@ -5,7 +5,6 @@ export const testRouter = new Router();
 
 testRouter.get('/api/test', (req,res,next) => {
     res.send('Hello');
-    return;
 });
 
 testRouter.post('/api/test/post', (req,res,next) => {
