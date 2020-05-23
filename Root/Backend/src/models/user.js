d35736d7e86593a2d054c69c0c23ea775ed6bbf9
Jordan Reichgut <jreichgut@gmail.com>
2020-05-23 12:34:07 -0400

    more merge conflicts

--- Root/Backend/src/models/user.js
+++ Root/Backend/src/models/user.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose';
 import { userSchema } from '../db/schemas.js'
   
 // Note that this syntax is required when using the ES6 import system.
 export const User = mongoose.model('user', userSchema);
-=======
-//Boilerplate code to create a User object for
-//MongoDB
-import mongoose from 'mongoose';
-import { userSchema } from '../db/schemas.js'
-  
-//Note that this syntax is required when using the ES6 import system.
-export const User = mongoose.model('user', userSchema);
-  
->>>>>>> master
