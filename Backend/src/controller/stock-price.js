7bb5dd910d6e904a48c2c214c861ad60d387932a
Alberto Oshiro <albertooshiro90@gmail.com>
2020-05-26 14:57:00 -0700

    await and async

--- Backend/src/controller/stock-price.js
+++ Backend/src/controller/stock-price.js
@@ -2,31 +2,28 @@ import dotenv from "dotenv";
 import "isomorphic-fetch"
 dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 
-
 function getStockURL(stock) {
 	return "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="+
 			stock + "&apikey=" + process.env.ALPHAVANTAGE_KEY;
 }
 
-function getStockPrices(stocks) {
-	var fetchCalls = [];
+async function getStockPrices(stocks) {
 	let stockPrices = new Map();
-	var i;
-	for (i = 0; i < stocks.length; i++) {
-		const stock = stocks[i];
-		const stockURL = getStockURL(stock);
-		const fetchCall = () => {
-			fetch(stockURL)
-				.then(response => response.json())
-				.then(data => {
-			  		const price = data["Global Quote"]["05. price"];
-			  		stockPrices[stock] = price;
-			  	});
+	const fetchCalls = stocks.map(stock => {
+		console.log("stockPrices");
+		const url = getStockURL(stock);
+		console.log(url);
+    	fetch(url)
+    		.then(response => response.json())
+    		.then(data => {
+		  		const price = data["Global Quote"]["05. price"];
+		  		stockPrices[stock] = price; })
+    		.catch((error) => {
+  				console.log("Can't find price for: " + stock + ".");
+			});
 		}
-		fetchCalls.push(fetchCall);
-	}
-	const fetchMap = async () => await Promise.all(fetchCalls);
-	fetchMap();
+  	);
+	await Promise.all(fetchCalls);
 	return stockPrices;
 }
 
--- Backend/src/routes/stock_routes.js
+++ Backend/src/routes/stock_routes.js
@@ -1,9 +1,14 @@
 import Router from 'express';
 import bodyParser from 'body-parser';
-import { getStockPrice } from "../controller/stock-price.js";
+import { getStockPrice, getStockPrices } from "../controller/stock-price.js";
 
 export const stockRouter = new Router();
 
 stockRouter.get('/price', (req,res) => {
 	getStockPrice(req, res);
+});
+
+stockRouter.get('/prices', (req, res) => {
+	console.log(getStockPrices(["IBM","MMM"]));
+	res.send("here");
 });
